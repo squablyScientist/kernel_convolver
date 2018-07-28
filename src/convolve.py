9abbdf9d8eba894c4d9eeaf871bf31824e64a649
Collin Tod <squablesquable@gmail.com>
2018-07-28 18:23:29 -0400

    initial commit

---
+++ src/convolve.py
@@ -0,0 +1,11 @@
+import matplotlib.image as mtimg
+import matplotlib.pyplot as plt
+import numpy as np
+
+kernel = [[-1, 0, 1],
+          [-2, 0, 2],
+          [-1, 0, 1]]
+
+img = mtimg.imread(input("Please enter filename"))
+
+
