d6e8ba14d2eba4ee8566b5460ec7a4efd7718dac
Collin Tod <squablesquable@gmail.com>
2018-07-29 13:13:45 -0400

    Experiment with blur and add back ReLU

--- convolve.py
+++ convolve.py
@@ -1,5 +1,7 @@
+import matplotlib.image as mpimg
 from skimage import io, color
 from skimage.viewer import ImageViewer
+import cv2
 import numpy as np
 import sys
 
@@ -9,7 +11,8 @@ import sys
 def convolve(kernel, img):
     output = np.empty_like(img)
 
-    img = np.pad(img, ((0, 0), (0, 0)), mode='edge')
+    # Pads the image by extending it by 1 pixel on all sides
+    img = np.pad(img, ((1, 1), (1, 1)), mode='edge')
 
     for i in range(img.shape[0] - 2):
         for j in range(img.shape[1] - 2):
@@ -17,22 +20,22 @@ def convolve(kernel, img):
             #A lot is happening here:
             # performs element-wise multilplication w/ a submatrix of the original image and the kernel
             # takes the sum of the resulting matrix
-            output[i, j] = (kernel * img[i:i+3, j:j+3]).sum()
+            output[i, j] =min( max((kernel * img[i:i+3, j:j+3]).sum(), 0), 255)
     return output
 
 
-sobely = [[-1, 0, 1],
+sobely =  np.array([[-1, 0, 1],
           [-2, 0, 2,],
-          [-1, 0, 1]]
+          [-1, 0, 1]])
 
-sobelx = [[-1, -2, -1],
+sobelx =  np.array([[-1, -2, -1],
           [0, 0, 0,],
-          [1, 2, 1]]
+          [1, 2, 1]])
 
-img = io.imread(sys.argv[1])
-img = color.rgb2gray(img)
+blur = (1/9) * np.array([[1,1,1],[1,1,1],[1,1,1]])
+img = cv2.imread(sys.argv[1], 0)
 
-convoluted = convolve(sobelx, img)
+convoluted = convolve(blur, img)
 ImageViewer(convoluted).show()
-
+io.imsave('test.png', convoluted)
 
