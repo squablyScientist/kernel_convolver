36d4f42e602f22e384a87acf417fd4440ac317f2
Collin Tod <squablesquable@gmail.com>
2018-07-29 13:50:42 -0400

    add full color kernel convolution

--- convolve.py
+++ convolve.py
@@ -12,15 +12,15 @@ def convolve(kernel, img):
     output = np.empty_like(img)
 
     # Pads the image by extending it by 1 pixel on all sides
-    img = np.pad(img, ((1, 1), (1, 1)), mode='edge')
+    img = np.pad(img, ((1, 1), (1, 1), (0, 0)), mode='edge')
 
     for i in range(img.shape[0] - 2):
         for j in range(img.shape[1] - 2):
-            
-            #A lot is happening here:
-            # performs element-wise multilplication w/ a submatrix of the original image and the kernel
-            # takes the sum of the resulting matrix
-            output[i, j] =min( max((kernel * img[i:i+3, j:j+3]).sum(), 0), 255)
+            for k in range(3): 
+                #A lot is happening here:
+                # performs element-wise multilplication w/ a submatrix of the original image and the kernel
+                # takes the sum of the resulting matrix
+                output[i, j, k] =min(max((kernel[k] * img[i:i+3, j:j+3, k]).sum(), 0), 255)
     return output
 
 
@@ -32,8 +32,16 @@ sobelx =  np.array([[-1, -2, -1],
           [0, 0, 0,],
           [1, 2, 1]])
 
-blur = (1/9) * np.array([[1,1,1],[1,1,1],[1,1,1]])
-img = cv2.imread(sys.argv[1], 0)
+blur = np.multiply(1/9, np.full((3, 3, 3), 1))
+        
+img = cv2.imread(sys.argv[1], 1)
+
+
+laplacian = np.full((3, 3), -1)
+laplacian[1, 1] = 8
+
+
+laplacian_color = [laplacian, laplacian, laplacian]
 
 convoluted = convolve(blur, img)
 ImageViewer(convoluted).show()
