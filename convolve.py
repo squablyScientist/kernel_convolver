c1ec2e8b01a025b797452ecda410f75317bb2708
Collin Tod <squablesquable@gmail.com>
2018-07-29 14:00:26 -0400

    Create adge detector for all 4 directions
    Note this is very inefficient the way that it is at the moment and can
    be improved

--- convolve.py
+++ convolve.py
@@ -20,22 +20,35 @@ def convolve(kernel, img):
             #A lot is happening here:
             # performs element-wise multilplication w/ a submatrix of the original image and the kernel
             # takes the sum of the resulting matrix
-            output[i, j] =min( max((kernel * img[i:i+3, j:j+3]).sum(), 0), 255)
+            output[i, j] = min(max((kernel * img[i:i+3, j:j+3]).sum(), 0), 255)
     return output
 
 
-sobely =  np.array([[-1, 0, 1],
+sobelx =  np.array([[-1, 0, 1],
           [-2, 0, 2,],
           [-1, 0, 1]])
 
-sobelx =  np.array([[-1, -2, -1],
+sobelx2 = np.flipud(sobelx)
+sobely =  np.array([[-1, -2, -1],
           [0, 0, 0,],
           [1, 2, 1]])
+
+sobely2 = np.flipud(sobely) 
 
 blur = (1/9) * np.array([[1,1,1],[1,1,1],[1,1,1]])
 img = cv2.imread(sys.argv[1], 0)
 
-convoluted = convolve(blur, img)
-ImageViewer(convoluted).show()
+convoluted = convolve(sobely, img)
+convoluted2 = convolve(sobely2, img)
+convoluted3 = convolve(sobelx, img)
+convoluted4 = convolve(sobelx2, img)
+
+# Combine both sobel
+convoluted_final = np.empty_like(img)
+for i in range(img.shape[0]):
+    for j in range(img.shape[1]):
+        convoluted_final[i,j] = (convoluted[i,j] + convoluted2[i,j])/2+ (convoluted3[i,j]+ convoluted4[i,j])/2
+
+ImageViewer(convoluted_final).show()
 io.imsave('test.png', convoluted)
 
